2852a4eee083b65baf48f363369c30dd333a3686
mariuszMarek <mariusz.marek.olszewski@gmail.com>
2015-03-05 18:43:24 +0100

    v1.3
    Dodane wyświetlanie prostych statystyk karty

--- wyswietlKarte.php
+++ wyswietlKarte.php
@@ -5,15 +5,32 @@ if(isset($_POST['wylogowanie']) == "1")	{$_SESSION["zalogowany"] = 0;}
 if(!isset($_SESSION["zalogowany"]))		{$_SESSION["zalogowany"] = 0;}
 if($_SESSION["zalogowany"] == 0)		{$wyswietl = 0;}
 
- ?>
- 
-<!DOCTYPE html>
-<?php 
 include_once("PHP/class.Zaleznosci.php");
-?>
+$kontener = new Kontener();
+
+if(isset($_POST['nrKarty']))
+{
+	$nrKarty = $_POST['nrKarty'];
+	$wyswietlKarte = Kontener::nowaKarta();
+	
+	if($wyswietlKarte->selectCard($nrKarty))
+	{	
+		$nick = $wyswietlKarte->getProperty("nick");
+		$lvl  = $wyswietlKarte->getProperty("lvl");
+		$epx  = $wyswietlKarte->getProperty("exp");	
+	}
+	else
+	{
+		unset($_POST['nrKarty']);
+	}
+}
+ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <html lang="pl">
   <head>
     <title>System obsługi kart Pubu EXP</title>
+	<meta http-equiv="content-type" content="text/html">
     <meta charset="UTF-8">
     <meta name="Description" content="skaner karty">
     <meta name="Keywords" content="karta, skaner, pub, autorski">
@@ -23,7 +40,7 @@ include_once("PHP/class.Zaleznosci.php");
   </head>
   <body>
         <div id="sidebar">
-             <h1><a href="index.php">Menu</a></h1>
+              <h1><a href="index.php">Menu</a></h1>
              <p> Wybierz poniżej co chcesz zrobić</p>
              <ol id="nay">
 				<li><a href="statystyki.php"><b>»</b>Statystyki</a></li>
@@ -36,6 +53,7 @@ include_once("PHP/class.Zaleznosci.php");
 					<li><a href="zapiszDoPliku.php"><b>»</b>Zapis bazy do pliku</a></li>
 					<li><a href="dodajNowaKarteDoBazy.php"><b>»</b>Dodaj nowego klienta</a></li>
 					<li><a href="usunKarteZBazy.php"><b>»</b>Usuń klienta</a></li>
+					<li><a href="rachunek.php"><b>»</b>Otwórz rachunek</a></li>
 					<li><b> </b>Zalogowany</li>
 					<form method = "post">
 					<input type ="hidden" name = "wylogowanie" value = "1">
@@ -46,13 +64,32 @@ include_once("PHP/class.Zaleznosci.php");
              </ol>
         </div>
         <div id="content">
-             <center>SKANER</center>
-			</br>
-			</br>
-			</br>
-			<table  width="500" height="1">
-				<tr> <td><li><a href="doliczPunkty.php"><font size="2">dolicz punkty do karty</font></a></li></td><td><li><font size="2"><a href="wyswietlKarte.php">odczytaj dane z karty</a></font></li></td> </tr>
-			</table>
+             <center>Główna Strona</center>
+				<br>
+				<br>
+				<br>
+			<hr>				
+				<form method="post">
+				<table>
+				<tr><td>Numer Karty</td><td><input type="number" min="0" max="99999" step="1" name = "nrKarty"></td><td><input type="submit" value ="Szukaj">  </td></tr>
+				
+				</form>
+				<?php 
+				if(isset($_POST['nrKarty']) and ($_POST['nrKarty'] != ""))
+				{
+				?>
+				
+						<tr><td>Nick</td>			<td><?php print $nick ?></td>
+						<tr><td>Lvl</td>			<td><?php print $lvl ?></td>
+						<tr><td>Exp</td>	<td><?php print $epx ?></td>
+						<tr><td>Nr Karty</td>	<td><?php print $nrKarty ?></td>
+					</table>
+				<?php
+				}
+				?>
+			</hr>
 		</div>
+
   </body>
 </html>
+
